c8442b017a96fe5fa7e89f34437cbd8a4252b8f3
ondastudio <hello@ondastudio.co>
2026-02-26 19:49:03 +0000

    update swiper navigation testimonials

--- public/swiper/swiper-testimonials.js
+++ public/swiper/swiper-testimonials.js
@@ -36,5 +36,21 @@ export function swiperTestimonials() {
         spaceBetween: 40,
       },
     },
+    on: {
+      init: function () {
+        // Set the total number of slides
+        document.querySelector("[swiper-total-number]").textContent =
+          this.slides.length;
+
+        // Set the initial current slide number (account for zero-based index)
+        document.querySelector("[swiper-current-number]").textContent =
+          this.activeIndex + 1;
+      },
+      slideChange: function () {
+        // Update the current slide number on slide change
+        document.querySelector("[swiper-current-number]").textContent =
+          this.activeIndex + 1;
+      },
+    },
   });
 }

eef5db08b7f8d9de714e04ee8eff5d782fd38c8f
ondastudio <hello@ondastudio.co>
2026-03-19 17:55:13 +0000

    add swiper highlights

---
+++ public/swiper/swiper-highlights.js
@@ -0,0 +1,52 @@
+export function swiperHighlights() {
+  const element = document.querySelector(
+    ".section_highlights"
+  );
+
+  if (!element) return;
+
+
+  var swiper_base = new Swiper(".swiper-highlights", {
+    slidesPerView: 2,
+    speed: 700,
+    spaceBetween: 40,
+    centeredSlides: true,
+    loop: false,
+    navigation: {
+      nextEl: ".swiper-highlights .customers-next-arrow",
+      prevEl: ".swiper-highlights .customers-prev-arrow",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    breakpoints: {
+      0: {
+        spaceBetween: 16,
+        slidesPerView: 1,
+      },
+      1024: {
+        spaceBetween: 40,
+      },
+      1400: {
+        spaceBetween: 40,
+      },
+    },
+    on: {
+      init: function () {
+        // Set the total number of slides
+        document.querySelector("[swiper-total-number]").textContent =
+          this.slides.length;
+
+        // Set the initial current slide number (account for zero-based index)
+        document.querySelector("[swiper-current-number]").textContent =
+          this.activeIndex + 1;
+      },
+      slideChange: function () {
+        // Update the current slide number on slide change
+        document.querySelector("[swiper-current-number]").textContent =
+          this.activeIndex + 1;
+      },
+    },
+  });
+}

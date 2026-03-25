79e12e1d6cb71aff91f6d85e1f4e145c9778b206
ondastudio <hello@ondastudio.co>
2026-03-25 17:46:08 +0000

    fix swiper highlights

--- public/swiper/swiper-highlights.js
+++ public/swiper/swiper-highlights.js
@@ -31,18 +31,16 @@ export function swiperHighlights() {
     },
     on: {
       init: function () {
-        // Set the total number of slides
-        document.querySelector("[swiper-total-number]").textContent =
-          this.slides.length;
-
-        // Set the initial current slide number (account for zero-based index)
+        const realTotal = this.slides.filter(
+          (s) => !s.classList.contains("swiper-slide-duplicate")
+        ).length;
+        document.querySelector("[swiper-total-number]").textContent = realTotal;
         document.querySelector("[swiper-current-number]").textContent =
-          this.activeIndex + 1;
+          this.realIndex + 1;
       },
       slideChange: function () {
-        // Update the current slide number on slide change
         document.querySelector("[swiper-current-number]").textContent =
-          this.activeIndex + 1;
+          this.realIndex + 1;
       },
     },
   });
